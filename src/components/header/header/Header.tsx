cd329a2067b9edbf9d8f7a7fa3b8745cbb19e6be
AndriiMoskalenko <andreymus0@gmail.com>
2023-05-11 13:58:00 +0300

    update: popout menu

--- src/components/header/header/Header.tsx
+++ src/components/header/header/Header.tsx
@@ -2,12 +2,23 @@ import Catalog from "../catalog/Catalog"
 import Basket from "../basket/Basket"
 import CartNavigation from "../cart/CartNavigation"
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
 import { authActions } from "@/store/authSlice"
 import { info } from "@/store/authActions"
 
 import classes from "./Header.module.css"
+import ShopName from "../title/ShopName"
+import SearchHeader from "../search/SearchHeader"
+import Sighup from "../signup/Signup"
+
+import Menu from "@/svg/Menu"
+import { CSSTransition } from "react-transition-group"
+
+const animationTiming = {
+  enter: 300,
+  exit: 300,
+}
 
 const Header = () => {
   const dispatch = useDispatch()
@@ -17,16 +28,64 @@ const Header = () => {
     dispatch(info())
   }, [dispatch])
 
+  const [toogle, setToogle] = useState(false)
+
+  const toogleHadnler = () => {
+    setToogle((prevState) => !prevState)
+  }
+  useEffect(() => {
+    document.body.style.overflow = toogle ? "hidden" : "auto"
+
+    return () => {
+      document.body.style.overflow = "auto"
+    }
+  }, [toogle])
+
   return (
     <>
-      <header className={classes.cart}>
-        <CartNavigation />
-        <Catalog />
-      </header>
-      <header className={classes.mobile}>
-        <div className={classes.left}>s</div>
-        <div className={classes.right}>s</div>
-      </header>
+      <>
+        <header className={classes.cart}>
+          <CartNavigation />
+          <Catalog />
+        </header>
+      </>
+      <>
+        <header className={classes.mobile}>
+          <div className={classes.left} onClick={toogleHadnler}>
+            <div className={classes.menu}>
+              <Menu />
+            </div>
+          </div>
+          <div className={classes.right}>
+            <ShopName />
+            <SearchHeader />
+            <Sighup />
+            <Basket />
+          </div>
+        </header>
+
+        <div>
+          {toogle && (
+            <div className={classes.background} onClick={toogleHadnler}></div>
+          )}
+          <CSSTransition
+            in={toogle}
+            timeout={animationTiming}
+            mountOnEnter
+            unmountOnExit
+            classNames={{
+              // enter: classes.fade_slide_enter,
+              enterActive: classes.fade_slide_enter_active,
+              // exit: classes.fade_slide_exit,
+              exitActive: classes.fade_slide_exit_active,
+            }}
+          >
+            <div className={classes.catalog}>
+              <div>ds</div>
+            </div>
+          </CSSTransition>
+        </div>
+      </>
     </>
   )
 }
---
+++ src/svg/Menu.tsx
@@ -0,0 +1,19 @@
+const Menu = () => {
+  return (
+    <svg
+      fill="white"
+      width={"75%"}
+      height={"75%"}
+      viewBox="0 0 52 52"
+      data-name="Layer 1"
+      id="Layer_1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M50,12.5H2a2,2,0,0,1,0-4H50a2,2,0,0,1,0,4Z" />
+      <path d="M50,28H2a2,2,0,0,1,0-4H50a2,2,0,0,1,0,4Z" />
+      <path d="M50,43.5H2a2,2,0,0,1,0-4H50a2,2,0,0,1,0,4Z" />
+    </svg>
+  )
+}
+
+export default Menu
